3dde6e65cff5f9543fc374d96580d1bde87c14dd
brianyu18 <brianyu1990@gmail.com>
2018-09-23 00:07:27 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,28 @@
+def instantiate_new_array
+  nu_array = []
+end
+
+def array_with_two_elements
+  nu_array = ['ele1', 'ele2']
+  return nu_array
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def first_element_with_array_methods(array)
+  return array.last
+end
